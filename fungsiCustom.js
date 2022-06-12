eb887a3a6253093cb649b2db92150dee9424ef6d
barkahharis <barkahris@gmail.com>
2022-06-12 14:39:02 +0700

    challenge 6

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,44 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  const arr = [];
+
+  fs.readFile(file1, { encoding: "utf-8" }, (err, data) => {
+    if (err) {
+      return fnCallback(err, null);
+    }
+
+    let ambilData = JSON.parse(data);
+    let ambilPesan = ambilData.message.split(" ");
+    arr.push(ambilPesan[1]);
+
+
+    fs.readFile(file2, { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        return fnCallback(err, null);
+      }
+
+      let ambilData = JSON.parse(data);
+      let ambilPesan = ambilData[0].message.split(" ");
+      arr.push(ambilPesan[1]);
+ 
+
+      fs.readFile(file3, { encoding: "utf-8" }, (err, data) => {
+        if (err) {
+          return fnCallback(err, null);
+        }
+
+          let ambilData = JSON.parse(data);
+          let ambilPesan = ambilData[0].data.message.split(" ");
+          arr.push(ambilPesan[1]);
+
+            return fnCallback(err, arr);
+      });
+    });
+  });
+};
+
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
